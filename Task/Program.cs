dc87b0f4f2e9622803f01a5237c3f7d948c5eed4
v <antyan2010@rambler.ru>
2023-02-14 16:53:44 +0300

    Addid Task about find string in array

---
+++ Task/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+Clear();
+
+string[] array = {"Hello", "2", "world", ":-)"};
+// string[] array1 = {"1234", "1567", "-2", "computer science"};
+// string[] array2 = {"Russia", "Denmark", "Kazan"};
+int b = LenghtNewArray(array);
+string[] newarray = NewArray(array, b);
+PrintArray(array);
+Write(" -> ");
+PrintArray(newarray);
+
+int LenghtNewArray(string[] getarr)
+{
+    int result = 0;
+    for (int i = 0; i < getarr.Length; i++)
+    {
+        if (getarr[i].Length <= 3) result++;
+    }
+    return result;
+}
+       
+string[] NewArray(string[] arr, int size)
+{
+    string[] resultarray = new string[size];
+    int c = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3){ resultarray[c] = arr[i]; c++;}
+    }
+    return resultarray;
+}
+
+
+void PrintArray(string[] prarr)
+{
+    if (prarr.Length > 0) {
+        Write("[");
+        for (int i = 0; i < prarr.Length - 1; i++)
+        {
+            Write($"'{prarr[i]}', ");
+        }
+        Write($"'{prarr[prarr.Length - 1]}']");}
+    else Write("[]");
+}
+
